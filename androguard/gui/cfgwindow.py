dd885c5a534382039f52f13fdeb0221a4073c98a
Frieder Steinmetz <frieder.steinmetz@gmail.com>
2019-06-21 09:16:31 +0200

    Comments and minor cleanup

--- androguard/gui/cfgwindow.py
+++ androguard/gui/cfgwindow.py
@@ -2,7 +2,7 @@ import hashlib
 import pydot
 import svgutils
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
-from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QScrollArea
+from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QScrollArea, QWidget
 from PyQt5.QtCore import Qt
 from androguard.gui.xrefwindow import XrefDialogString
 from androguard.core.bytecode import method2dot
@@ -19,57 +19,80 @@ class CfgWindow(QtWidgets.QWidget):
         self.current_class = current_class
         self.class_analysis = class_analysis
 
-        #Container Widget
-        widget = QtWidgets.QWidget()
-        #Layout of Container Widget
+        # wrapper widget for scrolling
+        scrollWidget = QWidget()
+        # layout for the scroll widget
         layout = QVBoxLayout(self)
 
         # create graphs for every method
         svgs = self.get_method_graphs()
         for i, svg in enumerate(svgs):
+            # use svgutils to get the SVG size attributes
             svgParsed = svgutils.transform.fromstring(svg.decode('utf-8'))
             svgWidget = CfgSVGWindow(svg, win)
-            svgWidget.setFixedHeight(int(svgParsed.height[:-2]))
-            svgWidget.setFixedWidth(int(svgParsed.width[:-2]))
+            # height and length are of the format "12pt" hence the [:-2]
+            svgWidget.setFixedHeight(int(svgParsed.height[:-2])*1.5)
+            svgWidget.setFixedWidth(int(svgParsed.width[:-2])*1.5)
             layout.addWidget(svgWidget)
-        # set layout to container widget
-        widget.setLayout(layout)
 
-        #Scroll Area Properties
-        scroll = QScrollArea()
-        scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        scroll.setWidgetResizable(False)
-        scroll.setWidget(widget)
+        # apply layout to wrapper widget
+        scrollWidget.setLayout(layout)
 
-        #Scroll Area Layer add
-        vLayout = QVBoxLayout(self)
-        vLayout.addWidget(scroll)
-        self.setLayout(vLayout)
+        # define a scroll area to actually make the widget scrollable
+        scrollArea = QScrollArea()
+        scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        scrollArea.setWidgetResizable(False)
+        # set scroll widget to scroll area
+        scrollArea.setWidget(scrollWidget)
+
+        # create a vbox layout and add scroll area
+        vBoxLayout = QVBoxLayout(self)
+        vBoxLayout.addWidget(scrollArea)
+        # apply the layout to this widget
+        self.setLayout(vBoxLayout)
 
     def get_method_graphs(self):
+        # get _the_ androguard ``Analysis`` object that contains our current class
         dx = self.mainwin.session.get_analysis(self.current_class)
-
+        # we're going to fill this svgs array with one svg graph per method
         svgs = []
+        # m is of type ``EncodedMethod```
         for m in self.current_class.get_methods():
+            # get a ``MethodAnalysis`` object for our method m
             mx = dx.get_method(m)
+            # get a graph of our method in dot format
             buff_dot = method2dot(mx)
             # unpack the returned graph list because we expect a single graph
-            (graph,) = self.create_graph(buff_dot, "png")
+            (graph,) = self.create_graph(buff_dot, str(m.get_name()))
+            # render svg
             svg = graph.create_svg()
             svgs.append(svg)
 
         return svgs
 
 
-    def create_graph(self, data, output):
-        # this is from the androguard source
+    def create_graph(self, data, name):
+        """
+        Constructs an actual CFG from dot output of method2dot and loads it with pydot
+
+        :param data: :string:
+        :param colors: dict of colors to use, if colors is None the default colors are used
+
+        :returns: pydot CFG
+        :rtype: class: ``pydot.Dot``
+        """
+        # this is from the bytecode.py
+        # pydot is optional!
+        import pydot
+
         buff = "digraph {\n"
         buff += "graph [rankdir=TB]\n"
         buff += "node [shape=plaintext]\n"
 
         # subgraphs cluster
-        buff += "subgraph cluster_" + hashlib.md5(output.encode('utf-8')).hexdigest() + " {\nlabel=\"%s\"\n" % data['name']
+        buff += "subgraph cluster_{} ".format(hashlib.md5(bytearray(name, "UTF-8")).hexdigest())
+        buff += "{\n"
+        buff += "label=\"{}\"\n".format(data['name'])
         buff += data['nodes']
         buff += "}\n"
 
